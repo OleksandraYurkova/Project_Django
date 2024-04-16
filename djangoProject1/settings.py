977daef0ec44c2bdb4a32116122366a5190d3ca7
Oleksandra Yurkova <117718362+shura1805@users.noreply.github.com>
2024-04-16 09:46:21 +0300

    11

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangoProject1.urls'
+ROOT_URLCONF = 'Test.urls'
 
 TEMPLATES = [
     {
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'PR06',
+        'USER': 'postgres',
+        'PASSWORD': 'MdkGme3ZaIoSXpc1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
